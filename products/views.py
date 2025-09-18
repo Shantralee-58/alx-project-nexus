b8553a758de5bebad57f47c0ea121bd4621f61c2
Idah Lindiwe Khumalo <86086701+Shantralee-58@users.noreply.github.com>
2025-09-18 16:15:20 +0200

    added Sustainify product seed data

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+# List all products OR create a new product
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+# Retrieve, update, or delete a single product
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
--- sustainify/settings.py
+++ sustainify/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',   # for APIs
+    'rest_framework',
     'users',
     'products',
     'categories',
