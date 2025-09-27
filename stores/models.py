49a1fa77dd52a3859095d2f3b114dd9de9a7377c
Idah Lindiwe Khumalo <86086701+Shantralee-58@users.noreply.github.com>
2025-09-27 23:42:53 +0200

    updating stores page

--- stores/models.py
+++ stores/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 class Store(models.Model):
-    name = models.CharField(max_length=100)
-    address = models.CharField(max_length=255, default='N/A')
-    city = models.CharField(max_length=100, default='Unknown City')
-    zip_code = models.CharField(max_length=10, default='0000')
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    postal_code = models.CharField(max_length=10)
 
     def __str__(self):
         return self.name
-    
-    def full_address(self):
-        return f"{self.address}, {self.city} {self.zip_code}"
+
+    class Meta:
+        # Orders stores alphabetically by name
+        ordering = ['name']
--- stores/views.py
+++ stores/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from .models import Store
 
 def store_list_view(request):
+    """
+    Displays a list of all sustainable stores from the database.
+    """
     stores = Store.objects.all()
     context = {
         'stores': stores
